d93726b56af3f12a73a635633e543073ac634509
giangthithuyluong <luonggiang1308@gmail.com>
2025-10-03 15:53:10 +0700

    find bug 1

--- popup/link.js
+++ popup/link.js
@@ -9,7 +9,12 @@ var src = './popup/'
 
 var firstScriptTag = document.getElementsByTagName('script')[0]
 // liên kết js
-;[`${src}data.js`, `${src}setup.js`, `${src}render.js`, `${src}popup.js`, `${src}openDialog.js`].forEach(src => {
+;[`${src}data.js`, `${src}setup.js`, `${src}render.js`].forEach(src => {
+    const script = document.createElement('script')
+    script.src = src
+    firstScriptTag.parentNode.appendChild(script)
+})
+;[`${src}popup.js`, `${src}openDialog.js`].forEach(src => {
     const script = document.createElement('script')
     script.src = src
     firstScriptTag.parentNode.appendChild(script)
